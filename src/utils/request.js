e48310f06ad71ca3d91b2a2b473f529507c3a257
hexuntao <hexuntao@foxmail.com>
2024-01-13 14:10:00 +0800

    dev

--- src/utils/request.js
+++ src/utils/request.js
@@ -35,6 +35,7 @@ service.interceptors.request.use(
     if (getToken() && !isToken) {
       config.headers['Authorization'] = 'Bearer ' + getToken(); // 让每个请求携带自定义token 请根据实际情况自行修改
     }
+    config.url = config.url + '?s=1';
     // get请求映射params参数
     if (config.method === 'get' && config.params) {
       let url = config.url + '?' + tansParams(config.params);
